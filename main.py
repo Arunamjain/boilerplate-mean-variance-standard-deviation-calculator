8a077d27c5237f289a77f0ae2c765488729a8cdd
Arunamjain <166137622+Arunamjain@users.noreply.github.com>
2026-05-03 18:01:44 +0530

    Refactor main.py to use calculate and unittest
    Updated entry point to import calculate and run tests.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import mean_var_std
-from unittest import main
+from mean_var_std import calculate
+import unittest
+import test_module
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+unittest.main(module='test_module', exit=False, verbosity=2)
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+# Quick manual test
+print(calculate([0,1,2,3,4,5,6,7,8]))
